f2871c06899383f9d0e8d9756f782f07d6a8bcbf
creativeaanya <creativeaanya2012@gmail.com>
2026-09-21 22:25:58 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+# Brighten a Day Project
+# Goal: Create an app that emails positive messages
+
+# Variables, Imports & Constants
+import random
+import pandas
+import smtplib
+
+SEND_FROM = ""
+SEND_TO = []
+
+data = pandas.read_csv("quotes.csv")
+
+# Functions
+def choose_quote(data_file):
+    quote_num = random.randint(0,19)
+    quote = data_file["Quotes"][quote_num]
+    speaker = data_file["Speaker"][quote_num]
+    formatted_quote = f'"{quote}"\n~{speaker}'
+    send_email(formatted_quote)
+    
+def send_email(quote_msg):
+    safe_msg = (
+        f"From: {SEND_FROM}\n"
+        "Subject: Motivational Quote\n"
+        "MIME-Version: 1.0\n"
+        "Content-Type: text/plain; charset=utf-8\n"
+        "\n"
+        f"{quote_msg}"
+    ).encode("utf-8")
+    
+    for addr in SEND_TO:
+        with smtplib.SMTP("smtp.mail.yahoo.com",587) as connection:
+            connection.starttls()
+            connection.login(user=SEND_FROM, password="")
+            connection.sendmail(from_addr=SEND_FROM, to_addrs=str(addr), msg=safe_msg)
+        
+# Executed Code 
+choose_quote(data)
